e7c895e066d5207c7d3d1bc26cd053c78bf24dbe
taehyun9244 <namtaehyun9244@gmail.com>
2022-12-20 23:06:30 +0900

    event form and validator

---
+++ src/main/java/com/example/mvcprac/controller/EventController.java
@@ -0,0 +1,9 @@
+package com.example.mvcprac.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class EventController {
+}
